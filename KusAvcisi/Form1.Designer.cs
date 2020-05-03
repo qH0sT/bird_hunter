88c2d6a80199d07192ae8cd73d86e359beae8c9b
qH0sT <asuslaptop230@gmail.com>
2020-05-03 17:10:10 +0300

    Bird Hunting

---
+++ KusAvcisi/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace KusAvcisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Olive;
+            this.label1.Location = new System.Drawing.Point(138, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Puan: 0";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(6, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mermi: 3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(138, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Kuş Sayısı: 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.label4.Location = new System.Drawing.Point(6, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Seviye: 1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(865, 425);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "nisan.png");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 325);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(865, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tablo";
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(738, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 72);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Çıkış Yap";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(614, 25);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 72);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Tekrar Başlat";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 425);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ KusAvcisi/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KusAvcisi
+{
+    public partial class Form1 : Form
+    {
+        //KARNIZMIZ TEVECCÜH'E BU KADAR TOK MU???! BEN AÇIM!! https://www.youtube.com/watch?v=eyyrkQo2XtY
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeResources();
+            Nisan();
+        }
+        List<Kus> kuslar = new List<Kus>();
+        Random rnd = new Random(Guid.NewGuid().GetHashCode());
+        int mermi = 3;
+        int puan = 0;
+        int seviye = 1;
+        public void Nisan()
+        {
+            Bitmap bmp = new Bitmap(ımageList1.Images[0],new Size(50,50));
+            bmp.MakeTransparent();
+            pictureBox2.Cursor = new Cursor(bmp.GetHicon());
+        }
+        class Kus
+        {
+            public int sayac = 0;
+            PictureBox kus = new PictureBox();
+            Form1 frm = null;
+            public Kus(Form1 frm1)
+            {
+                frm = frm1;
+                kus.Location = new Point(12, 44);
+                //kus.BringToFront();
+                kus.Name = "pictureBox1";
+                kus.Size = new Size(55, 50);
+                kus.ImageLocation = Environment.CurrentDirectory + "\\Res\\bird.gif";
+                kus.SizeMode = PictureBoxSizeMode.Zoom;
+                kus.TabIndex = 0;
+                kus.TabStop = false;
+                kus.BackColor = Color.Transparent;
+                kus.Click += new EventHandler(vurus);
+                frm1.pictureBox2.Controls.Add(kus);
+            }
+            private void vurus(object sender, EventArgs args)
+            {
+                if (frm.mermi > 0)
+                {
+                    //MessageBox.Show("Vurdun!!!");
+                    SoundPlayer sp = new SoundPlayer(Environment.CurrentDirectory + "\\Res\\shot.wav");
+                    sp.Play();
+                    frm.puan += 5;
+                    frm.label1.Text = "Puan: "+frm.puan.ToString();
+                    frm.pictureBox2.Controls.Remove(kus);                    
+                    frm.kuslar.Remove(this);
+                    frm.mermi -= 1;
+                    frm.mermi_ += 1;
+                    frm.label2.Text = "Mermi: "+frm.mermi.ToString();
+                    frm.label3.Text = "Kuş Sayısı: " + frm.kuslar.Count.ToString();
+                    //frm.Text = frm.kuslar.Count.ToString();
+                }
+                else {
+                MessageBox.Show("Mermin bitti :((","Mermi Sayısı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    frm.timer1.Stop();
+                    frm.timer2.Stop();
+                    frm.pictureBox2.Controls.Clear();
+                }
+            }
+            
+            public void Hareket(int down, int up)
+            {
+                kus.Left += frm.rnd.Next(1,20); //Kuşun tekdüze hareket etmesini önlemek için Random sınıfı ile dalgalı bir uçuş sağlıyoruz.
+                if (sayac <= down)
+                {
+                    kus.Top += 10;
+                }
+                else
+                {
+                    kus.Top -= 10;
+                    if (sayac == up)
+                    {
+                        sayac = 0;
+                    }
+                }
+                sayac++;
+            }
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            /*
+            if (mermi > 0)
+            {
+                mermi -= 1;
+                label2.Text = mermi.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Mermin yok :((");
+            }
+            */
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //kuşları hareket ettiren timer
+        {
+            foreach (Kus kusumuz in kuslar)
+            {
+                kusumuz.Hareket(15, 30);
+            }
+        }
+        int kus_sayisi = 0;
+        int mermi_ = 0;
+        private async void timer2_Tick(object sender, EventArgs e)
+        {
+            if(kuslar.Count != 0)
+            {
+                MessageBox.Show("Kaybettin!!!","Süre Doldu",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                timer2.Stop();
+                timer1.Stop();
+            }
+            else
+            {
+                //timer1.Stop();
+                timer2.Stop();
+                //MessageBox.Show("LEVEL");
+                
+                if (timer1.Interval > 10) { timer1.Interval -= 10; }
+                if (timer2.Interval < 30000) { timer2.Interval += 10000; };
+                int g = (kus_sayisi + 5);
+                mermi = mermi_ + g + 1;
+                mermi_ = 0;
+                label2.Text = "Mermi: " + mermi.ToString();
+                label4.Text = "Seviye: " + (seviye += 1);
+                for (int j =0; j < g ; j++)
+                {
+                    Kus bird = new Kus(this);
+                    kuslar.Add(bird);
+                    kus_sayisi += 1;
+                    
+                    for (int k = 0; k < 5; k++)
+                    {
+                        bird.Hareket(15, 30);
+                        await Task.Delay(250);
+                    }
+                    label3.Text = "Kuş Sayısı: " + kuslar.Count.ToString();
+                }
+
+                label3.Text = "Kuş Sayısı: " + kuslar.Count.ToString();
+                //timer1.Start();
+                timer2.Start();
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            for (int h = 0; h < 3; h++)
+            {
+                Kus bird = new Kus(this);
+                kuslar.Add(bird);
+                kus_sayisi += 1;
+                for (int k = 0; k < 5; k++)
+                {
+                    bird.Hareket(15, 30);
+                    await Task.Delay(250);
+                }
+            }
+        }
+        private void InitializeResources()
+        {
+            //label1.BackColor = System.Drawing.Color.Transparent;
+            pictureBox2.ImageLocation = Environment.CurrentDirectory + "\\Res\\jungle_background.jpg";
+            pictureBox1.Top = groupBox1.Top + 120;
+            pictureBox1.ImageLocation = Environment.CurrentDirectory + "\\Res\\hunter_back.jpg";
+            //pictureBox1.ImageLocation = Environment.CurrentDirectory + "\\Res\\jungle_background.jpg";
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (mermi > 0)
+            {
+                mermi -= 1;
+                mermi_ += 1;
+                label2.Text = "Mermi: "+ mermi.ToString();
+                SoundPlayer sp = new SoundPlayer(Environment.CurrentDirectory + "\\Res\\shot.wav");
+                sp.Play();
+            }
+            else
+            {
+                MessageBox.Show("Mermin bitti :((", "Mermi Sayısı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                timer1.Stop();
+                timer2.Stop();
+                pictureBox2.Controls.Clear();
+        
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            //pictureBox1.Left = e.X;
+            //Avcıyı hareket ettiren event ama kasma yaptığı için iptal ettim. İsterseniz yorum işaretini kaldırıp aktif edebilirsiniz.
+        }
+    }
+}
